239e9e9e7ab153a9aaa8aa7090442a5706fbb25c
surabhi555 <pednekarsurabhi555@gmail.com>
2025-01-02 18:51:49 +0530

    added layout of frontend

---
+++ frontend/src/features/notes/NotesLists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotesLists = () => {
+  return (
+    <h1>NotesLists</h1>
+  )
+}
+
+export default NotesLists
---
+++ frontend/src/features/users/UsersLists.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UsersLists = () => {
+  return (
+    <h1>UsersLists</h1>
+  )
+}
+
+export default UsersLists
